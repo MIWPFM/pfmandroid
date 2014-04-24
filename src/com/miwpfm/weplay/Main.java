823e1a4a1cd8e5dd5b3f42331ae5de235857f0e5
7Alberto7 <j.albertocg86@gmail.com>
2014-04-24 19:25:49 +0200

    Subida inicial

---
+++ src/com/miwpfm/weplay/Main.java
@@ -0,0 +1,14 @@
+package com.miwpfm.weplay;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
