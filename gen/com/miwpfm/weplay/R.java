fc8a19e64fac9f8c53c77985dd90922875b95129
7Alberto7 <j.albertocg86@gmail.com>
2014-05-12 22:04:17 +0200

    Añadido el navigation drawer con estilos y sus fragments asociados

--- gen/com/miwpfm/weplay/R.java
+++ gen/com/miwpfm/weplay/R.java
@@ -25,31 +25,66 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
         public static final int activity_horizontal_margin=0x7f060000;
         public static final int activity_vertical_margin=0x7f060001;
+        public static final int navigation_drawer_width=0x7f060002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_action_overflow=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int selector=0x7f020004;
     }
     public static final class id {
-        public static final int config_notif=0x7f0a0001;
-        public static final int find_game=0x7f0a0000;
-        public static final int share_app=0x7f0a0002;
+        public static final int action_overflow=0x7f0a0006;
+        public static final int config_notif=0x7f0a0008;
+        public static final int container=0x7f0a0001;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int find_game=0x7f0a0007;
+        public static final int icon=0x7f0a0003;
+        public static final int navigation_drawer=0x7f0a0002;
+        public static final int section_label=0x7f0a0005;
+        public static final int share_app=0x7f0a0009;
+        public static final int text=0x7f0a0004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_home=0x7f030000;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int fragment_home=0x7f030002;
+        public static final int fragment_mygames=0x7f030003;
+        public static final int fragment_myinfo=0x7f030004;
+        public static final int fragment_mymessages=0x7f030005;
+        public static final int fragment_mysports=0x7f030006;
+        public static final int fragment_navigation_drawer=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f090000;
+        public static final int home=0x7f090000;
     }
     public static final class string {
+        public static final int action_example=0x7f07000d;
+        public static final int action_settings=0x7f07000e;
         public static final int buscar_partido=0x7f070001;
         public static final int compartir_app=0x7f070003;
         public static final int configurar_notificaciones=0x7f070002;
+        public static final int menu_option_home=0x7f070005;
+        public static final int menu_option_logout=0x7f07000a;
+        public static final int menu_option_mygames=0x7f070007;
+        public static final int menu_option_myinfo=0x7f070006;
+        public static final int menu_option_mymessages=0x7f070009;
+        public static final int menu_option_mysports=0x7f070008;
+        public static final int navigation_drawer_close=0x7f07000c;
+        public static final int navigation_drawer_open=0x7f07000b;
+        public static final int title_activity_home=0x7f070004;
         public static final int weplay=0x7f070000;
     }
     public static final class style {
@@ -59,6 +94,7 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
         Base application theme for API 11+. This theme completely replaces
